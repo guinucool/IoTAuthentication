51a2be9fbbe4cf720f0b407f1bd0d653c59952f2
jipee03 <gugalima.43r@gmail.com>
2025-02-15 20:26:39 +0100

    Create handler.py
    updates

---
+++ handler.py
@@ -0,0 +1,31 @@
+from message import Message
+
+
+class Handler:
+    def __init__(handler, DB, request, response):
+
+        handler.DB = DB
+        handler.request = request
+        handler.response = response
+
+
+    def writeMessage():
+     
+
+        return null
+
+
+
+
+    class DB:
+        def __init__(self, id_disp, session_id, sensors, timestamp):
+            
+            self.id_disp = id_disp
+            self.session_id = session_id
+            self.sensors = sensors
+            self.timestamp = timestamp
+
+        
+        def 
+
+        
