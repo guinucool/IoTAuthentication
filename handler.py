296a94cd41d3d72c581fa6c7e300836664b20a75
jipee03 <112565178+jipee03@users.noreply.github.com>
2025-02-15 20:24:29 +0000

    Update handler.py

--- handler.py
+++ handler.py
@@ -2,28 +2,83 @@ from message import Message
 
 
 class Handler:
-    def __init__(handler, DB, request, response):
-
+    def __init__(handler, DB, request, device_id, session_id, sensors, timestamp):
         handler.DB = DB
         handler.request = request
-        handler.response = response
+        handler.device_id = device_id
+        handler.session_id = session_id
+        handler.sensors = sensors
+        handler.timestamp = timestamp
 
 
-    def writeMessage():
-     
+    def handleMessage(handler):
+        '''
+        Handles the received message. This message can be an Authentication message or an Information msg
+        Depending on the received message a determined action will occur
+        '''
+        if self.request.get("type") == "authentication":
+            return self.handle_authentication()
+        elif self.request.get("type") == "information":
+            return self.handle_information()
+        else:
+            return {"status": "error", "message": "Invalid message type"}
+        
 
-        return null
+    def handle_authentication(self):
+        '''
+        (?)
+        '''
 
 
+    def handle_information(self):
+        '''
+        (?)
+        '''
+        
+
+    def __check_session_id(handler, session_id: int) -> bool:
+        '''
+        Checks if the given identifier is the one supposed to be received in a message.
+
+        Args:
+            device_id (int): The identifier to check.
+
+        Returns:
+            bool: The result of checking.
+        '''
+
+
+    def __check_device_id(handler, device_id: int) -> bool:
+        '''
+        Checks if the given identifier is the one supposed to be received in a message.
+
+        Args:
+            session_id (int): The identifier to check.
+
+        Returns:
+            bool: The result of checking.
+        '''
+
+    def __store_data(handler):
+        '''
+        Stores sensor data in the database.
+        '''
 
 
     class DB:
-        def __init__(self, id_disp, session_id, sensors, timestamp):
-            
-            self.id_disp = id_disp
-            self.session_id = session_id
-            self.sensors = sensors
-            self.timestamp = timestamp
+        def __init__(self):
+            '''
+                Initializes the DataBase Object.
+
+                Args:
+                    devices (int): The identifier of the device the authenticator keeps track of.
+                    sessions (int): The identifier of the session the authenticator keeps track of.
+                    sensors(?)
+            '''
+           
+        
+        
+
 
         
         def 
